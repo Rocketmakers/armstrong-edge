b10a83f5c5f1036fb170563a6155a9a1f1b7179a
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-06-11 15:58:08 +0100

    fix typo in useTemporaryState

--- module/src/hooks/useTemporaryState.ts
+++ module/src/hooks/useTemporaryState.ts
@@ -3,15 +3,15 @@ import * as React from 'react';
 import { useTimeout } from './useTimeout';
 
 /**
- * useState but the state returns to initialValue after a given amount of time from whenever setState is caleld
- * @param intialValue the initial value of the state - the value gets changed back to this after the amount of time given
+ * useState but the state returns to initialValue after a given amount of time from whenever setState is called
+ * @param initialValue the initial value of the state - the value gets changed back to this after the amount of time given
  * @param time the amount of time in ms to wait to return back to initialValue
  */
 
-export function useTemporaryState<T>(intialValue?: T, time = 500): [T | undefined, (newValue: T) => void] {
-  const [value, setValue] = React.useState(intialValue);
+export function useTemporaryState<T>(initialValue?: T, time = 500): [T | undefined, (newValue: T) => void] {
+  const [value, setValue] = React.useState(initialValue);
 
-  const onTimeout = React.useCallback(() => setValue(intialValue), []);
+  const onTimeout = React.useCallback(() => setValue(initialValue), []);
 
   const { set: setTimeout } = useTimeout(time, onTimeout);
 
