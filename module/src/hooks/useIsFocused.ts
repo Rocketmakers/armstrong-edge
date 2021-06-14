6055fc90fb9c503e5cebd64fa5b76d487a77c99d
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-06-14 11:02:48 +0100

    allow useIsFocused and useIsHovering hooks to be used with a ref

--- module/src/hooks/useIsFocused.ts
+++ module/src/hooks/useIsFocused.ts
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+import { useEventListener } from './useEventListener';
+
 export type UseIsFocusedReturn = [boolean, Pick<React.HTMLAttributes<HTMLElement>, 'onFocus' | 'onBlur'>];
 
-/** Get, in React state, whether an element is being focused, using the props returned from the second element in the returned array */
-export const useIsFocused = (): UseIsFocusedReturn => {
+/**
+ * Get, in React state, whether an element is being focused.
+ * Can be used by spreading the props returned from the second element in the returned array onto the element you want to listen to,
+ * or by passing in a ref
+ */
+export const useIsFocused = (ref?: React.MutableRefObject<Element | undefined | null>): UseIsFocusedReturn => {
   const [isFocused, setIsFocused] = React.useState(false);
 
   const onFocus = React.useCallback(() => {
@@ -13,6 +19,9 @@ export const useIsFocused = (): UseIsFocusedReturn => {
   const onBlur = React.useCallback(() => {
     setIsFocused(false);
   }, []);
+
+  useEventListener('focus', onFocus, ref?.current || undefined);
+  useEventListener('blur', onBlur, ref?.current || undefined);
 
   return [isFocused, { onFocus, onBlur }];
 };
--- module/src/hooks/useIsHovering.ts
+++ module/src/hooks/useIsHovering.ts
@@ -1,9 +1,15 @@
 import * as React from 'react';
 
+import { useEventListener } from '.';
+
 export type UseIsHoveringReturn = [boolean, Pick<React.HTMLAttributes<HTMLElement>, 'onMouseEnter' | 'onMouseLeave'>];
 
-/** Get, in React state, whether an element is being hovered, using the event listener props returned from the second element in the returned array */
-export function useIsHovering(): UseIsHoveringReturn {
+/**
+ * Get, in React state, whether an element is being hovered.
+ * Can be used by spreading the props returned from the second element in the returned array onto the element you want to listen to,
+ * or by passing in a ref
+ */
+export function useIsHovering(ref?: React.MutableRefObject<Element | undefined | null>): UseIsHoveringReturn {
   const [isHovering, setIsHovering] = React.useState(false);
 
   const onMouseEnter = React.useCallback(() => {
@@ -13,6 +19,9 @@ export function useIsHovering(): UseIsHoveringReturn {
   const onMouseLeave = React.useCallback(() => {
     setIsHovering(false);
   }, []);
+
+  useEventListener('mouseEnter', onMouseEnter, ref?.current || undefined);
+  useEventListener('mouseLeave', onMouseLeave, ref?.current || undefined);
 
   return [isHovering, { onMouseEnter, onMouseLeave }];
 }
