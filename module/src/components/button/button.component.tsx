a1ce4ba8c6d2fcce2d2ca20dac112597cbef8e8e
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-06-15 13:28:01 +0100

    make button forward a ref

--- module/src/components/button/button.component.tsx
+++ module/src/components/button/button.component.tsx
@@ -8,7 +8,7 @@ import { ValidationErrors } from '../validationErrors';
 
 export interface IButtonProps
   extends IIconWrapperProps<IconSet, IconSet>,
-    React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+    Omit<React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>, 'ref'>,
     IStatusWrapperProps {
   /** (string) CSS className property */
   className?: string;
@@ -30,46 +30,52 @@ export interface IButtonProps
 }
 
 /** Renders an HTML button element with some useful additions */
-export const Button: React.FC<IButtonProps> = ({
-  className,
-  validationErrorMessages,
-  errorIcon,
-  pending,
-  disabled,
-  error,
-  leftIcon,
-  rightIcon,
-  children,
-  statusPosition,
-  hideIconOnStatus,
-  ...nativeProps
-}) => {
-  const shouldShowErrorIcon = !!validationErrorMessages?.length || error;
+export const Button = React.forwardRef<HTMLButtonElement, IButtonProps>(
+  (
+    {
+      className,
+      validationErrorMessages,
+      errorIcon,
+      pending,
+      disabled,
+      error,
+      leftIcon,
+      rightIcon,
+      children,
+      statusPosition,
+      hideIconOnStatus,
+      ...nativeProps
+    },
+    ref
+  ) => {
+    const shouldShowErrorIcon = !!validationErrorMessages?.length || error;
 
-  const showLeftIcon = statusPosition !== 'left' || !hideIconOnStatus || (!pending && !shouldShowErrorIcon);
-  const showRightIcon = statusPosition !== 'right' || !hideIconOnStatus || (!pending && !shouldShowErrorIcon);
+    const showLeftIcon = statusPosition !== 'left' || !hideIconOnStatus || (!pending && !shouldShowErrorIcon);
+    const showRightIcon = statusPosition !== 'right' || !hideIconOnStatus || (!pending && !shouldShowErrorIcon);
 
-  return (
-    <>
-      <button
-        {...nativeProps}
-        className={ClassNames.concat('arm-button', className)}
-        data-pending={pending}
-        data-disabled={disabled || pending}
-        data-error={error || validationErrorMessages?.length}
-        disabled={disabled || pending}
-      >
-        <IconWrapper leftIcon={showLeftIcon ? leftIcon : undefined} rightIcon={showRightIcon ? rightIcon : undefined}>
-          <StatusWrapper errorIcon={errorIcon} statusPosition={statusPosition} error={error} validationErrorMessages={validationErrorMessages}>
-            {children}
-          </StatusWrapper>
-        </IconWrapper>
-      </button>
+    return (
+      <>
+        <button
+          {...nativeProps}
+          className={ClassNames.concat('arm-button', className)}
+          data-pending={pending}
+          data-disabled={disabled || pending}
+          data-error={error || validationErrorMessages?.length}
+          disabled={disabled || pending}
+          ref={ref}
+        >
+          <IconWrapper leftIcon={showLeftIcon ? leftIcon : undefined} rightIcon={showRightIcon ? rightIcon : undefined}>
+            <StatusWrapper errorIcon={errorIcon} statusPosition={statusPosition} error={error} validationErrorMessages={validationErrorMessages}>
+              {children}
+            </StatusWrapper>
+          </IconWrapper>
+        </button>
 
-      {!!validationErrorMessages?.length && <ValidationErrors validationErrors={validationErrorMessages} icon={errorIcon} />}
-    </>
-  );
-};
+        {!!validationErrorMessages?.length && <ValidationErrors validationErrors={validationErrorMessages} icon={errorIcon} />}
+      </>
+    );
+  }
+);
 
 Button.defaultProps = {
   errorIcon: IconUtils.getIconDefinition('Icomoon', 'warning'),
