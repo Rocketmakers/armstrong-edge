aee5a271883fb7b5db66cd29ffb58febc88d27ea
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-06-24 11:35:25 +0100

    document lifecycle hooks, add useMatchMedia hook

--- module/src/hooks/useDidMountEffect.ts
+++ module/src/hooks/useDidMountEffect.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/** A useEffect which will only run in the first effect */
 export function useDidMountEffect(callback: () => void) {
   React.useEffect(callback, []);
 }
--- module/src/hooks/useWillUnmountEffect.ts
+++ module/src/hooks/useWillUnmountEffect.ts
@@ -1,5 +1,6 @@
 import React from 'react';
 
+/** A useEffect which only runs on the cleanup of the last effect */
 export function useWillUnMountEffect(callback: () => void) {
   React.useEffect(() => callback, []);
 }
