ef259cb36320f44b10313a5f041bff35197b1d42
Joe <joe@rocketmakers.com>
2021-06-25 18:11:47 +0100

    fix: deepscan issue in playground fixed because I want that green tick

--- playground/src/views/examples/time.tsx
+++ playground/src/views/examples/time.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { TimeInput, Form } from "@rocketmakers/armstrong-edge"
 
 export const TimeExample: React.FC = () => {
-  const { formProp, formState } = Form.use<{ myDate: string }>()
+  const { formProp } = Form.use<{ myDate: string }>()
 
   return <TimeInput bind={formProp("myDate").bind()}  />
 }
