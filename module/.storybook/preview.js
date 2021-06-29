35189028b01fa40814389c3d644277184dcc38ac
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-06-29 11:07:27 +0100

    add show code by default

--- module/.storybook/preview.js
+++ module/.storybook/preview.js
@@ -35,3 +35,35 @@ export const parameters = {
     },
   },
 };
+
+// Show code in Docs view by default, from https://github.com/storybookjs/storybook/issues/10430
+
+window.addEventListener('load', () => {
+  let loc = window.location.href;
+  showCodeSamples();
+
+  window.setInterval(() => {
+    let newLoc = window.location.href;
+
+    if (newLoc !== loc) {
+      loc = newLoc;
+      showCodeSamples();
+    }
+  }, 300);
+});
+
+function showCodeSamples() {
+  try {
+    const docs = document.querySelectorAll('.sbdocs');
+
+    [].forEach.call(docs, (el) => {
+      const buttons = el.querySelectorAll('button');
+      const codeButton = [].find.call(buttons, (el) => el.textContent === 'Show code');
+      if (codeButton) {
+        codeButton.click();
+      }
+    });
+  } catch (e) {
+    console.warn(e);
+  }
+}
