44b9c9d09b5023e05c720565a28fd253441875f6
Joe <joe@rocketmakers.com>
2021-07-29 16:29:59 +0100

    fix: ARM-36 fixed issue in which broken React import syntax was causing typing file errors

--- module/src/components/dialog/dialog.hooks.tsx
+++ module/src/components/dialog/dialog.hooks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 
 import { Button } from '../button';
 import { IUseModalLayerPromiseComponentProps, useModalLayerPromise } from '../modal/modal.hooks';
