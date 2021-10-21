6de82f164d7f7ae44f4f60a47e35575a354ca108
patrickbenjamin1 <crumbleshake@live.co.uk>
2021-10-21 10:45:37 +0100

    feature: make useTimeout return a waiting state, make set from useTimeout return a promise, and let you override time when calling set

--- module/src/hooks/useTimeout.ts
+++ module/src/hooks/useTimeout.ts
@@ -1,11 +1,16 @@
 import * as React from 'react';
 
-export interface IUseTimeoutReturn {
+import { useWillUnMountEffect } from '.';
+
+export interface IUseTimeoutReturn<T = void> {
   /** Set the timeout, optionally give a callback which will override the one set at hook level */
-  set: (callback?: () => void) => void;
+  set: (callback?: () => T, time?: number) => Promise<T>;
 
   /** Clear the timeout before its time has finished */
   clear: () => void;
+
+  /** Has the timeout been triggered, but not elapsed */
+  waiting: boolean;
 }
 
 /**
@@ -13,27 +18,38 @@ export interface IUseTimeoutReturn {
  * @param time the time to pass before the callback is fired
  * @param callback the callback to run after the given time
  */
-export function useTimeout(time?: number, callback?: () => void): IUseTimeoutReturn {
+export function useTimeout<T = void>(callback?: () => T, time?: number): IUseTimeoutReturn<T> {
   const timeout = React.useRef<any>();
 
+  const [waiting, setWaiting] = React.useState(false);
+
+  const resolvePromise = React.useRef<(value: T | PromiseLike<T>) => void>();
+
+  const clear = React.useCallback(() => {
+    clearTimeout(timeout.current);
+    resolvePromise.current?.(undefined as any as T);
+    resolvePromise.current = undefined;
+  }, []);
+
   const set = React.useCallback(
-    (overrideCallback?: () => void) => {
-      if (overrideCallback || callback) {
-        timeout.current = setTimeout(overrideCallback ?? callback!, time);
-      }
+    (overrideCallback?: () => T, overrideTime?: number) => {
+      clear();
+
+      return new Promise<T>((resolve) => {
+        setWaiting(true);
+        resolvePromise.current = resolve;
+
+        timeout.current = setTimeout(() => {
+          setWaiting(false);
+          const value = (overrideCallback ?? callback)?.();
+          resolve(value as T);
+        }, overrideTime ?? time);
+      });
     },
     [time, callback]
   );
 
-  const clear = React.useCallback(() => {
-    clearTimeout(timeout.current);
-  }, []);
+  useWillUnMountEffect(clear);
 
-  React.useEffect(() => {
-    return () => {
-      clear();
-    };
-  }, []);
-
-  return { set, clear };
+  return { set, clear, waiting };
 }
