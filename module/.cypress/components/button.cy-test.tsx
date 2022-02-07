1a407426b7382ba2fedc48bcfc493ca9a907d9ea
Judith Owens <judith@rocketmakers.com>
2022-02-07 18:35:55 +0000

    feat(test) Finalises framework set-up

--- module/.cypress/components/button.cy-test.tsx
+++ module/.cypress/components/button.cy-test.tsx
@@ -7,11 +7,12 @@ import * as stories from '../../src/components/button/button.stories';
 const { Default, Pending } = composeStories(stories);
 
 it('should render a default button', () => {
-  mount(<Default />);
-  cy.get('span').should('contain', 'Click me please');
+  mount(<Default onClick={cy.stub().as('click')} />);
+  cy.get('button').should('have.text', Default.args?.children).click();
+  cy.get('@click').should('have.been.calledOnce');
 });
 
 it('should render a pending button', () => {
   mount(<Pending />);
-  cy.get('span').should('contain', 'Click me please');
+  cy.get('button').should('contain', Pending.args?.children).and('be.disabled');
 });
