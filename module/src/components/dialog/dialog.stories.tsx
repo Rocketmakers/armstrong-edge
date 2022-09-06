d428bef72ede42e0a9a63a5b57384ec4f0c0dde0
Joe Lewsey <joe@rocketmakers.com>
2022-09-06 14:06:03 +0100

    feat: allows confirmation dialog content to be overriden when the dialog is opened

--- module/src/components/dialog/dialog.stories.tsx
+++ module/src/components/dialog/dialog.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Button, useConfirmationDialog } from '../..';
+import { Button, useConfirmationDialog, useDispatchToast } from '../..';
 import { StoryUtils } from '../../stories/storyUtils';
 import { IconUtils } from '../icon';
 import { Dialog } from './dialog.component';
@@ -71,6 +71,25 @@ export const CustomCloseIcon = () => {
   );
 };
 
+export const ConfirmationDialog = () => {
+  const dispatch = useDispatchToast();
+
+  // Dialog config passed into the hook can be overridden on a per-property basis when the open method is called.
+  const [open] = useConfirmationDialog({
+    content: 'As an example, this content will be overridden when the dialog is called',
+    buttons: { yes: 'OK', no: 'Cancel' },
+  });
+
+  const onClick = React.useCallback(async () => {
+    const okToContinue = await open({ content: 'Are you sure you want to do that thing?' });
+    if (okToContinue) {
+      dispatch({ content: 'Thing done successfully', type: 'success' });
+    }
+  }, [open, dispatch]);
+
+  return <Button onClick={onClick}>Do a thing</Button>;
+};
+
 export const HandleClose = () => {
   const [open, setOpen] = React.useState(false);
 
@@ -80,7 +99,13 @@ export const HandleClose = () => {
     <>
       <Button onClick={() => setOpen(true)}>open dialog</Button>
 
-      <Dialog isOpen={open} onOpenChange={setOpen} onClose={() => openConfirmationDialog()}>
+      <Dialog
+        isOpen={open}
+        onOpenChange={setOpen}
+        onClose={() => {
+          void openConfirmationDialog();
+        }}
+      >
         Try and close me
       </Dialog>
     </>
