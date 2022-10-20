81ca091c3a3ab9777c7fbc4b1d2dd30528f2b481
Harry Smith <harrysmith@Harrys-MacBook-Pro-2.local>
2022-10-20 11:25:47 +0100

    chore: ARM-ARM-205 remove Webpack and replace with Vite in Storybook module. Update node version to 16.14.2

--- module/.cypress/plugins/index.js
+++ module/.cypress/plugins/index.js
@@ -1,9 +1,9 @@
-const injectDevServer = require('@cypress/react/plugins/load-webpack');
+import injectDevServer from '@cypress/react/plugins/load-webpack';
 
 /**
  * @type {Cypress.PluginConfig}
  */
-module.exports = (on, config) => {
+export default (on, config) => {
   if (config.testingType === 'component') {
     injectDevServer(on, config, { webpackFilename: './.cypress/webpack.config.js' });
   }
--- module/.storybook/main.js
+++ module/.storybook/main.js
@@ -1,59 +1,27 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  stories: ['../../src/**/*.stories.mdx', '../../src/**/*.stories.@(js|jsx|ts|tsx)', '../../docs/**/*.stories.mdx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y',
+    '@storybook/test-runner',
+  ],
+  framework: '@storybook/react',
   core: {
-    builder: 'webpack5',
+    builder: '@storybook/builder-vite',
   },
-  staticDirs: [],
-  webpackFinal: async (config, { configType }) => {
-    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-    // You can change the configuration based on that.
-    // 'PRODUCTION' is used when building the static version of storybook.
-
-    const isDevelopment = configType === 'DEVELOPMENT';
-
-    config.module.rules.push({
-      test: /\.s?css$/,
-      use: [
-        'style-loader',
-        {
-          loader: 'css-loader', // turn url() and @import calls into require
-          options: {
-            sourceMap: isDevelopment,
-          },
-        },
-        {
-          loader: 'postcss-loader',
-          options: {
-            sourceMap: isDevelopment,
-            postcssOptions: {
-              implementation: require('postcss'),
-              plugins: [require('autoprefixer')],
-            },
-          },
-        },
-        {
-          loader: 'sass-loader', // compile sass
-          options: {
-            sourceMap: isDevelopment,
-          },
-        },
-      ],
-    });
-
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        path: require.resolve('path-browserify'),
-      },
+  features: {
+    interactionsDebugger: true,
+  },
+  async viteFinal(config, { configType }) {
+    config.resolve.alias = {
+      '~@rocketmakers': './node_modules/@rocketmakers',
     };
-
-    // Return the altered config
+    config.css = {};
+    config.css.modules = {
+      localsConvention: 'dashesOnly',
+    };
     return config;
   },
-  babel: async (options) => ({
-    ...options,
-    plugins: [['@babel/plugin-transform-typescript', { isTSX: true, allExtensions: true, allowNamespaces: true }]],
-  }),
 };
