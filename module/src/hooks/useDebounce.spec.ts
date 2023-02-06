b1cba5525cb5cc33ddc986e46be742bff65af1e4
Judith Owens <judith@rocketmakers.com>
2023-02-06 10:52:25 +0000

    feat: ARM-191 adds useDebounce and useTemporaryState tests

---
+++ module/src/hooks/useDebounce.spec.ts
@@ -0,0 +1,141 @@
+import { act, renderHook, waitFor } from "@testing-library/react";
+import { useDebounce, useDebounceEffect } from "./useDebounce";
+
+describe("useDebounce", () => {
+    beforeEach(() => {
+        jest.useRealTimers();
+    });
+
+  it("debounces a value being passed in after a given time", async () => {
+    jest.useFakeTimers();
+    const ms = 600;
+    const value = 'test';
+    const middleValue = 'fish';
+    const newValue = 'value';
+    const onChange = jest.fn(() => null);
+    const { result } = renderHook(() => useDebounce(ms, value, onChange));
+
+    expect(result.current[0]).toBe(value);
+    expect(result.current[2]).toBe(value);
+
+    act(() => {
+      result.current[1](middleValue);
+      jest.advanceTimersByTime(ms - 1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(middleValue);
+      expect(result.current[2]).toBe(value);
+    });
+
+    act(() => {
+      result.current[1](newValue);
+      jest.advanceTimersByTime(ms - 1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+      expect(result.current[2]).toBe(value);
+    });
+
+    act(() => {
+      jest.advanceTimersByTime(1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+      expect(result.current[2]).toBe(newValue);
+    });
+
+    expect(onChange).toHaveBeenCalledWith(newValue);
+  });
+  
+  it("resets a value back to its original value if a reset is called", async () => {
+    const ms = 600;
+    const value = 'test';
+    const newValue = 'value';
+    const onChange = jest.fn(() => null);
+    const { result, rerender } = renderHook((prop) => useDebounce(ms, value, onChange));
+
+    expect(result.current[0]).toBe(value);
+    expect(result.current[2]).toBe(value);
+
+    act(() => {
+      result.current[1](newValue);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+      expect(result.current[2]).toBe(newValue);
+    });
+
+    act(() => {
+      result.current[3]();
+    });
+    rerender();
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(value);
+      expect(result.current[2]).toBe(value);
+    });
+  });
+
+  it("force sets a new value if the hard value changes", async () => {
+    const ms = 600;
+    let value = 'test';
+    const newValue = 'value';
+    const onChange = jest.fn(() => null);
+    const { result, rerender } = renderHook(() => useDebounce(ms, value, onChange));
+
+    expect(result.current[0]).toBe(value);
+    expect(result.current[2]).toBe(value);
+
+    act(() => {
+      result.current[1](newValue);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+      expect(result.current[2]).toBe(newValue);
+    });
+
+    act(() => {
+      value = 'fish';
+    });
+
+    rerender();
+    await waitFor(() => {
+      expect(result.current[0]).toBe(value);
+      expect(result.current[2]).toBe(value);
+    });
+  });
+});
+
+describe("useDebounceEffect", () => {
+
+  it("debounces an effect being triggered by a dependency change to a given time", async () => {
+    jest.useFakeTimers();
+    const ms = 600;
+    let value = 'test';
+    const newValue = 'value';
+    const onChange = jest.fn(() => null);
+    const { rerender } = renderHook(() => useDebounceEffect(onChange, ms, [value]));
+
+    act(() => {
+      value = newValue;
+      jest.advanceTimersByTime(ms - 1);
+    });
+
+    rerender();
+    expect(onChange).not.toBeCalled();
+
+    act(() => {
+      jest.advanceTimersByTime(1);
+    });
+
+    rerender();
+    await waitFor(() => {
+      expect(onChange).toBeCalled();
+    });
+  });
+});
---
+++ module/src/hooks/useTemporaryState.spec.ts
@@ -0,0 +1,72 @@
+import { act, renderHook, waitFor } from "@testing-library/react";
+import { useTemporaryState } from "./useTemporaryState";
+
+describe("useTemporaryState", () => {
+    beforeEach(() => {
+        jest.useRealTimers();
+    });
+
+  it("resets a value back to its original after a given time", async () => {
+    jest.useFakeTimers();
+    const ms = 600;
+    const value = 'test';
+    const newValue = 'value';
+    const onReset = jest.fn(() => null);
+    const { result } = renderHook(() => useTemporaryState(value, ms, onReset));
+
+    expect(result.current[0]).toBe(value);
+
+    act(() => {
+      result.current[1](newValue);
+      jest.advanceTimersByTime(ms - 1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+    });
+
+    expect(onReset).not.toHaveBeenCalled();
+
+    act(() => {
+      jest.advanceTimersByTime(1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(value);
+    });
+
+    expect(onReset).toHaveBeenCalled();
+  });
+
+  it("allows a user to reset back to the initial value before the given time", async () => {
+    jest.useFakeTimers();
+    const ms = 600;
+    const value = 'test';
+    const newValue = 'value';
+    const onReset = jest.fn(() => null);
+    const { result } = renderHook(() => useTemporaryState(value, ms, onReset));
+
+    expect(result.current[0]).toBe(value);
+
+    act(() => {
+      result.current[1](newValue);
+      jest.advanceTimersByTime(1);
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(newValue);
+    });
+
+    expect(onReset).not.toHaveBeenCalled();
+
+    act(() => {
+      result.current[2]();
+    });
+
+    await waitFor(() => {
+      expect(result.current[0]).toBe(value);
+    });
+
+    expect(onReset).not.toHaveBeenCalled();
+  });
+});
