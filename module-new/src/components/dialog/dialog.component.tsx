24c9c389e9f10074c5f2e07cdb67bd77d674d6ec
Joe Lewsey <joe@rocketmakers.com>
2023-04-18 15:29:53 +0100

    feat: ARM-282 - Added new dialogs to module-new + added some extra accessibility

---
+++ module-new/src/components/dialog/dialog.component.tsx
@@ -0,0 +1,160 @@
+import * as RadixDialog from '@radix-ui/react-dialog';
+import * as React from 'react';
+
+import { useCompareValues } from '../../hooks/useCompareValues';
+import { ArmstrongFCExtensions, ArmstrongFCProps, ArmstrongFCReturn } from '../../types';
+import { concat } from '../../utils/classNames';
+import { useArmstrongConfig } from '../config';
+
+import './dialog.theme.css';
+
+/** Dialog component props */
+export interface IDialogProps<TData = unknown> extends React.PropsWithChildren {
+  /** Optional title to show at the top of the dialog in an H2 tag */
+  title?: React.ReactNode;
+  /** Optional description to show in the body of the dialog in a P tag */
+  description?: React.ReactNode;
+  /** Icon to use as the close button. Send `false` to hide the close button entirely */
+  closeButtonIcon?: JSX.Element | false;
+  /** Bool denoting whether the dialog is open or closed - for state controlled dialogs */
+  open?: boolean;
+  /** Function called when the dialog is opened/closed - for state controlled dialogs */
+  onOpenChange?: (open: boolean) => void;
+  /** Optional CSS class for the dialog component */
+  className?: string;
+  /** Optional CSS class for the dialog overlay */
+  overlayClassName?: string;
+  /** Optional Data to pass into the dialog. Data will be returned from the async dialog functions allowing for reusable form based dialogs */
+  data?: TData;
+  /** Function called when the dialog is closed. Don't use for state control, only for side effects. `onOpenChange` should be used for state control */
+  onClose?: () => void;
+}
+
+/** String union describing the various actions that can be used to close a dialog */
+export type DialogFinishAction = keyof Omit<DialogElement<unknown>, 'open'>;
+
+/** The response type of the promise returned by the dialog `open` function  */
+export interface IDialogOpenResponse<TData = unknown> {
+  /** The action that caused the dialog to close */
+  action: DialogFinishAction;
+  /** Any data sent into the dialog `data` prop */
+  data?: TData;
+}
+
+/** Dialog element toolkit - these functions are assigned to the ref passed into the dialog */
+export interface DialogElement<TData = unknown> {
+  /** Opens the dialog and returns a promise which resolves when the dialog closes */
+  open: () => Promise<IDialogOpenResponse<TData>>;
+  /** Closes the dialog with a "close" finish action */
+  close: () => void;
+  /** Closes the dialog with an "ok" finish action */
+  ok: () => void;
+  /** Closes the dialog with a "cancel" finish action */
+  cancel: () => void;
+}
+
+/**
+ * Dialog component. Used to display a full-screen modal dialog to the user.
+ * - Can be state controlled with `open` and `onOpenChange` props.
+ * - Can be async by assigning a ref and calling the utility functions (a `useDialog` helper hook is available for this.)
+ * - Supports dynamic data in async mode, so that a form can be built as a reusable async dialog.
+ */
+export const Dialog = React.forwardRef(
+  (props: IDialogProps<unknown>, ref: React.ForwardedRef<DialogElement<unknown>>) => {
+    const {
+      children,
+      title,
+      description,
+      closeButtonIcon,
+      open,
+      onOpenChange,
+      onClose,
+      className,
+      data,
+      overlayClassName,
+    } = props;
+
+    /** Pull globals from context */
+    const globals = useArmstrongConfig({ dialogCloseButtonIcon: closeButtonIcon });
+
+    /** Finish action state is set when the dialog is closed */
+    const [finishAction, setFinishAction] = React.useState<DialogFinishAction | undefined>(open ? undefined : 'close');
+
+    /** Stores a reference to the promise resolver function */
+    const resolverRef =
+      React.useRef<(value: IDialogOpenResponse<unknown> | PromiseLike<IDialogOpenResponse<unknown>>) => void>();
+
+    /** Used to create prop comparisons to use as effect triggers */
+    const finishActionChanged = useCompareValues(finishAction);
+    const openHasChanged = useCompareValues(open);
+
+    /** Called when the internal open/close state of the dialog changes */
+    const onInnerOpenChange = React.useCallback(
+      (val: boolean) => {
+        setFinishAction(val ? undefined : 'close');
+      },
+      [setFinishAction]
+    );
+
+    /** Exposes the DialogElement utility functions to the ref */
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        open: () =>
+          new Promise(res => {
+            onInnerOpenChange(true);
+            resolverRef.current = res;
+          }),
+        close: () => setFinishAction('close'),
+        ok: () => setFinishAction('ok'),
+        cancel: () => setFinishAction('cancel'),
+      }),
+      [onInnerOpenChange, setFinishAction]
+    );
+
+    /** Listens to the `finishAction` state and runs the appropriate functions */
+    React.useEffect(() => {
+      if (finishAction && resolverRef.current) {
+        resolverRef.current({ action: finishAction, data });
+        resolverRef.current = undefined;
+      }
+      if (finishActionChanged) {
+        onOpenChange?.(!finishAction);
+        if (finishAction) {
+          onClose?.();
+        }
+      }
+    }, [finishActionChanged, finishAction, data, onOpenChange, onClose]);
+
+    /** Listens to changes on the incoming `open` prop for controlled dialogs, and runs the appropriate functions  */
+    React.useEffect(() => {
+      if (openHasChanged && open !== undefined) {
+        onInnerOpenChange(open);
+      }
+    }, [open, onInnerOpenChange, openHasChanged]);
+
+    return (
+      <RadixDialog.Root open={!finishAction} onOpenChange={onInnerOpenChange}>
+        <RadixDialog.Portal>
+          <RadixDialog.Overlay className={concat('arm-dialog-overlay', overlayClassName)}>
+            <RadixDialog.Content className={concat('arm-dialog', className)} data-has-title={title ? true : undefined}>
+              {title && <RadixDialog.Title className="arm-dialog-title">{title}</RadixDialog.Title>}
+              {description && (
+                <RadixDialog.Description className="arm-dialog-description">{description}</RadixDialog.Description>
+              )}
+              {children && <div className="arm-dialog-content">{children}</div>}
+              {globals.dialogCloseButtonIcon !== false && (
+                <RadixDialog.Close className="arm-dialog-close">{globals.dialogCloseButtonIcon}</RadixDialog.Close>
+              )}
+            </RadixDialog.Content>
+          </RadixDialog.Overlay>
+        </RadixDialog.Portal>
+      </RadixDialog.Root>
+    );
+  }
+  // type assertion to ensure generic works with RefForwarded component
+  // DO NOT CHANGE TYPE WITHOUT CHANGING THIS, FIND TYPE BY INSPECTING React.forwardRef
+) as (<TData>(props: ArmstrongFCProps<IDialogProps<TData>, DialogElement<TData>>) => ArmstrongFCReturn) &
+  ArmstrongFCExtensions<IDialogProps<unknown>>;
+
+Dialog.displayName = 'Dialog';
---
+++ module-new/src/hooks/useCompareValues.ts
@@ -0,0 +1,12 @@
+import { usePreviousValue } from './usePreviousValue';
+
+/**
+ * Compares an incoming prop or potential dependency to it's value on the previous render
+ * - Useful for forming an effect trigger in the world of exhaustive dependencies
+ * @param value The incoming prop or potential dependency
+ * @returns `true` if the value has changed since the previous render, else `false`.
+ */
+export const useCompareValues = <TValue>(value: TValue): boolean => {
+  const prevVal = usePreviousValue(value);
+  return prevVal !== value;
+};
