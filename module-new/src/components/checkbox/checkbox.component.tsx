63a919528b1c8f92a5eb0c216e08ff00d9370521
Tom Hinsley <tom.hinsley@rocketmakers.com>
2023-04-21 12:33:26 +0100

    feat: ARM-278 updated checkbox to use global variables and css, double check4ed validation errors, changed cursor to reflect only clickable sections,

--- module-new/src/components/checkbox/checkbox.component.tsx
+++ module-new/src/components/checkbox/checkbox.component.tsx
@@ -1,53 +1,61 @@
 import { CheckboxProps, Indicator, Root } from '@radix-ui/react-checkbox';
 import * as React from 'react';
 
-import { IBindingProps, useBindingState } from '../../hooks/form';
+import { IBindingProps, useBindingState, ValidationMessage } from '../../hooks/form';
 import { ArmstrongFCExtensions, ArmstrongFCProps, ArmstrongFCReturn, NullOrUndefined } from '../../types';
 import { concat } from '../../utils/classNames';
 import { useArmstrongConfig } from '../config';
 import { Label } from '../label/label.component';
-import { ValidationErrors } from '../validationErrors/validationErrors.component';
+import { StatusWrapper } from '../statusWrapper/statusWrapper.component';
+import { IValidationErrorsProps, ValidationErrors } from '../validationErrors/validationErrors.component';
 
 import './checkbox.theme.css';
 
 type BindType = NullOrUndefined<boolean | 'indeterminate'>;
 
 export interface ICheckboxProps<TData extends BindType>
-  extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'type' | 'checked'> {
-  /** An IBindingProps<TData> object to bind the checkbox input to a form. (Optional) */
+  extends Omit<React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLDivElement>, HTMLDivElement>, 'type' | 'checked'>,
+    Omit<IValidationErrorsProps, 'validationMode' | 'validationErrors'> {
+  /** (Optional) An IBindingProps<TData> object to bind the checkbox input to a form. */
   bind?: IBindingProps<TData>;
 
-  /** A TData value representing the initial checked state of the checkbox. This can be true, false, or 'indeterminate'. (Optional) */
+  /** (Optional) A TData value representing the initial checked state of the checkbox. This can be true, false, or 'indeterminate'. */
   checked?: TData;
 
-  /** A custom className to style the checkbox container. (Optional) */
-  className?: string;
+  /** (Optional) A custom className to style the checkbox container. */
+  checkboxClassName?: string;
 
-  /** A custom JSX.Element for the indeterminate state indicator. (Optional) */
+  /** (Optional) A custom JSX.Element for the indeterminate state indicator. */
   customIndeterminateIndicator?: JSX.Element;
 
-  /** A custom JSX.Element for the checked indicator. (Optional) */
+  /** (Optional) A custom JSX.Element for the checked indicator. */
   customIndicator?: JSX.Element;
 
-  /** A string representing the direction of the label relative to the checkbox. Default value is 'horizontal'. (Optional) */
+  /** (Optional) A string representing the direction of the label relative to the checkbox. Default value is 'horizontal'. */
   direction?: 'vertical' | 'horizontal';
 
-  /** A boolean flag to disable the checkbox input. (Optional) */
+  /** (Optional) A boolean flag to disable the checkbox input. */
   disabled?: boolean;
 
-  /** A React.ReactNode to display a label for the checkbox input. (Optional) */
+  /** (Optional) A React.ReactNode to display a label for the checkbox input. */
   label?: React.ReactNode;
 
-  /** Class name for the label component */
+  /** (Optional) Class name for the label component */
   labelClassName?: string;
 
-  /** A callback function (newValue: TData) => void to handle state when 'checked' is changed. (Optional) */
+  /** (Optional) A callback function (newValue: TData) => void to handle state when 'checked' is changed. */
   onCheckedChange?: (newValue: TData) => void;
 
-  /** A string to set a custom data-testid attribute for the checkbox container. (Optional) */
+  /** (Optional) A string to set a custom data-testid attribute for the checkbox container. */
   testId?: string;
 
-  /** A boolean flag to automatically scroll validation error messages into view. (Optional) */
+  /** (Optional) Classname for the validation errors */
+  validationErrorsClassName?: string;
+
+  /** (Optional) Can be a string or {key, element} key is necessary for animating in new messages   */
+  validationErrorMessages?: ValidationMessage[];
+
+  /** (Optional) A boolean flag to automatically scroll validation error messages into view. */
   scrollValidationErrorsIntoView?: boolean;
 }
 
@@ -55,7 +63,7 @@ export const Checkbox = React.forwardRef(
   <TData extends BindType>(
     {
       bind,
-      className,
+      checkboxClassName,
       checked,
       customIndicator,
       customIndeterminateIndicator,
@@ -66,39 +74,51 @@ export const Checkbox = React.forwardRef(
       labelClassName,
       scrollValidationErrorsIntoView,
       testId,
+      validationErrorsClassName,
+      validationErrorMessages,
       ...nativeProps
     }: ICheckboxProps<TData>,
     ref: React.ForwardedRef<HTMLButtonElement>
   ) => {
     const id = React.useId();
 
-    const [value, setValue, bindConfig] = useBindingState(bind, { onChange: onCheckedChange, value: checked });
-
-    const onCheckedChangeInternal = React.useCallback<Required<CheckboxProps>['onCheckedChange']>(
-      newValue => {
-        setValue?.(newValue as TData);
-      },
-      [setValue]
-    );
-
     const globals = useArmstrongConfig({
+      scrollValidationErrorsIntoView,
       checkboxCustomIndicator: customIndicator,
       checkboxCustomIndeterminateIndicator: customIndeterminateIndicator,
     });
 
+    const [boundValue, setBoundValue, bindConfig] = useBindingState(bind, {
+      value: checked,
+      onChange: onCheckedChange,
+      validationErrorMessages,
+      validationMode: globals.validationMode,
+    });
+
+    const onCheckedChangeInternal = React.useCallback<Required<CheckboxProps>['onCheckedChange']>(
+      newValue => {
+        setBoundValue?.(newValue as TData);
+      },
+      [setBoundValue]
+    );
+
     const indicator = React.useMemo(() => {
-      switch (value) {
+      switch (boundValue) {
         case 'indeterminate':
           return globals.checkboxCustomIndeterminateIndicator;
         default:
           return globals.checkboxCustomIndicator;
       }
-    }, [value, globals.checkboxCustomIndicator, globals.checkboxCustomIndeterminateIndicator]);
+    }, [boundValue, globals.checkboxCustomIndicator, globals.checkboxCustomIndeterminateIndicator]);
 
     return (
-      <>
+      <StatusWrapper
+        className={concat(validationErrorsClassName, 'arm-input-base')}
+        validationMode={bindConfig.validationMode}
+        errorIcon={bindConfig.validationErrorIcon}
+      >
         <div
-          className={concat('arm-checkbox-container', className)}
+          className={concat('arm-checkbox-container', checkboxClassName)}
           data-disabled={disabled}
           data-direction={direction}
           data-testid={testId}
@@ -108,7 +128,7 @@ export const Checkbox = React.forwardRef(
             className="arm-checkbox"
             disabled={disabled}
             id={id}
-            checked={value ?? undefined}
+            checked={boundValue ?? undefined}
             onCheckedChange={onCheckedChangeInternal}
             ref={ref}
           >
@@ -116,20 +136,27 @@ export const Checkbox = React.forwardRef(
               {indicator}
             </Indicator>
           </Root>
+
           {label && (
-            <Label className={concat('arm-checkbox-label', labelClassName)} data-direction={direction} htmlFor={id}>
+            <Label
+              className={concat('arm-checkbox-label', labelClassName)}
+              data-direction={direction}
+              data-disabled={disabled}
+              htmlFor={id}
+            >
               {label}
             </Label>
           )}
         </div>
         {!!bindConfig.validationErrorMessages?.length && bindConfig.shouldShowValidationErrorMessage && (
           <ValidationErrors
+            className={validationErrorsClassName}
+            validationMode={'message'}
             validationErrors={bindConfig.validationErrorMessages}
-            validationMode={bindConfig.validationMode}
-            scrollIntoView={scrollValidationErrorsIntoView}
+            scrollIntoView={globals.scrollValidationErrorsIntoView}
           />
         )}
-      </>
+      </StatusWrapper>
     );
   }
 ) as (<TBind extends NullOrUndefined<boolean>>(
--- module-new/src/components/checkbox/checkbox.stories.tsx
+++ module-new/src/components/checkbox/checkbox.stories.tsx
@@ -60,3 +60,21 @@ export const CustomIndicator: Story = {
     customIndicator: <ImRocket />,
   },
 };
+
+export const ValidationError: Story = {
+  args: {
+    label: 'Check',
+    direction: 'vertical',
+    testId: 'checkbox-container',
+  },
+  render: () => {
+    <h2>Validation mode - both</h2>;
+    return <Checkbox label={'Option 1'} validationErrorMessages={['This field is required']} />;
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const checkboxContainer = await canvas.getByTestId('checkbox-container');
+    const direction = await checkboxContainer.getAttribute('data-direction');
+    expect(direction).toBe('vertical');
+  },
+};
