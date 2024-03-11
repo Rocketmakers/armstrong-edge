d32b0d8b1a0116c9140c2d14862e2fe46ddc5c43
jlews <jlews@users.noreply.github.com>
2024-03-11 11:02:08 +0000

    deploy: db9628d835db24dd4eea6746953f447ba7b5a083

---
+++ assets/usePreviousValue.stories-c93d2833.js
@@ -0,0 +1,11 @@
+import{M as a}from"./chunk-HLWAVYOI-dc335b1c.js";import{j as o,a as p,F as i}from"./jsx-runtime-eae7a151.js";import{u as n}from"./index-f875e932.js";import"./iframe-5372f3b1.js";import"../sb-preview/runtime.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-d3dde439.js";import"./index-07d1f67e.js";import"./chunk-ZGA76URP-2b404cd8.js";import"./isNativeReflectConstruct-30e719dd.js";import"./index-11d98b33.js";import"./uniq-d447bef6.js";import"./_baseIsEqual-62e1ad13.js";import"./index-ec0b3b5e.js";import"./index-356e4a49.js";function s(r){const e=Object.assign({h3:"h3",p:"p",pre:"pre",code:"code"},n(),r.components);return p(i,{children:[o(a,{title:"Hooks/usePreviousValue"}),`
+`,o(e.h3,{id:"usepreviousvalue",children:"usePreviousValue"}),`
+`,o(e.p,{children:"Will return the previous value for an incoming prop or potential dependency for comparison"}),`
+`,o(e.pre,{children:o(e.code,{className:"language-tsx",children:`const previousValue = usePreviousValue(props.value);
+
+React.useEffect(() => {
+  if (previousValue != props.value) {
+    // do something
+  }
+}, [previousValue, props.value]);
+`})})]})}function u(r={}){const{wrapper:e}=Object.assign({},n(),r.components);return e?o(e,{...r,children:o(s,{...r})}):s(r)}const m=()=>{throw new Error("Docs-only story")};m.parameters={docsOnly:!0};const t={title:"Hooks/usePreviousValue",tags:["stories-mdx"],includeStories:["__page"]};t.parameters=t.parameters||{};t.parameters.docs={...t.parameters.docs||{},page:u};const w=["__page"];export{w as __namedExportsOrder,m as __page,t as default};
--- assets/useResizeObserver.stories-ad03eb19.js
+++
@@ -1,9 +0,0 @@
-import{M as i}from"./chunk-HLWAVYOI-6fc732ee.js";import{j as r,a,F as c}from"./jsx-runtime-eae7a151.js";import{u as o}from"./index-f875e932.js";import"./iframe-f7393f58.js";import"../sb-preview/runtime.js";import"./index-c4905b50.js";import"./_commonjsHelpers-042e6b4d.js";import"./react-18-d3dde439.js";import"./index-07d1f67e.js";import"./chunk-ZGA76URP-2b404cd8.js";import"./isNativeReflectConstruct-30e719dd.js";import"./index-11d98b33.js";import"./uniq-d447bef6.js";import"./_baseIsEqual-62e1ad13.js";import"./index-ec0b3b5e.js";import"./index-356e4a49.js";function n(t){const e=Object.assign({h3:"h3",p:"p",pre:"pre",code:"code"},o(),t.components);return a(c,{children:[r(i,{title:"Hooks/useResizeObserver"}),`
-`,r(e.h3,{id:"useresizeobserver",children:"useResizeObserver"}),`
-`,r(e.p,{children:"Use an resize observer to fire the passed callback - also cleans up on unmount. Can either be used by just passing in a ref, or by using the functions returned to observe and disconnect"}),`
-`,r(e.pre,{children:r(e.code,{className:"language-tsx",children:`const ref = React.useRef<HTMLDivElement>();
-
-useResizeObserver(onIntersect, ResizeObserverOptions, ref);
-
-return <div ref={ref} />;
-`})})]})}function p(t={}){const{wrapper:e}=Object.assign({},o(),t.components);return e?r(e,{...t,children:r(n,{...t})}):n(t)}const m=()=>{throw new Error("Docs-only story")};m.parameters={docsOnly:!0};const s={title:"Hooks/useResizeObserver",tags:["stories-mdx"],includeStories:["__page"]};s.parameters=s.parameters||{};s.parameters.docs={...s.parameters.docs||{},page:p};const D=["__page"];export{D as __namedExportsOrder,m as __page,s as default};
